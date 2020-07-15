7fede357080c2a23ba776408f84c79032e9b355c
henrichsen <41309272+henrichsen@users.noreply.github.com>
2020-07-15 13:50:40 -0600

    Update model_input_utils.py

--- tethysapp/ueb_app/model_input_utils.py
+++ tethysapp/ueb_app/model_input_utils.py
@@ -2,9 +2,9 @@
 Utilities for model input preparation web services
 """
 
-from epsg_list import EPSG_List
-from hydrods_model_input import *
-from user_settings import *
+from .epsg_list import EPSG_List
+from .hydrods_model_input import *
+#from .user_settings import *
 import json
 
 
